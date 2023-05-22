3ba0282e24a145b772986f59b63d0ca508cf0c93
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-22 21:42:10 +0100

    Removing old files

--- helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>                                                             // Library to input/output (io) in the terminal
-
-int main()                                                                      // Special function that contains body
-{
-    std::cout << "Hello world!" << std::endl                                    // std:: allows to use cout and endl without using their longer form 
-              << "My name is Nicolo" << std::endl;                              // cout allows to print to screen       
-
-    return 0;                                                                   // Return 0 (int) to tell computer if the program executed correctly
-}
--- makefile
+++
@@ -1,7 +0,0 @@
-# run makefile with: make all
-
-all:                                                                            # Compile
-	c++ functions_header.cpp -o functions_header
-	./functions_header
-clean: 																			# Remove the executable
-	rm functions_header
