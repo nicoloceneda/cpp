8699be70447e27062c2c400e3490ce1a9fe74238
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-22 21:54:13 +0100

    Data types

---
+++ 3-data_types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char letter = 'A';                                                                              // Single character
+    cout << letter << endl;
+
+    string phrase = "Pharase";                                                                      // More than one character
+    cout << phrase << endl;
+
+    int number_int = 50;                                                                            // Integer
+    cout << number_int << endl;
+
+    double number_float = 50.1;                                                                     // Decimal
+    cout << number_float << endl;
+
+    bool is_true = true;                                                                            // Boolean
+    cout << is_true << endl;
+
+    return 0;
+}
