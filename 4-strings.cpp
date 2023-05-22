859218a604280012b90d3c231f2fda33a3aac532
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-22 23:26:02 +0100

    Strings

---
+++ 4-strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string phrase = "My number is 0123456789";
+    cout << "String: " << phrase << endl;
+
+    cout << "Length of the string: " << phrase.length() << endl;                                    // Length of string
+
+    phrase[13] = 'A';                                                                               // Replace a character
+    cout << "Modified string: " << phrase << endl;
+
+    string phrasesub = phrase.substr(3, 6);                                                         // Slice a string
+    cout << "Sliced string: " << phrasesub << endl;                                                 // - First argument is strting index
+                                                                                                    // - Second argument is length
+
+    string word = "number";                                                                         // Check for character/string:
+    cout << "Index where " << word << " apprears: " << phrase.find(word, 0) << endl;                // - Second argument is index were it start looking                
+                                                                                                    // - Output is index were character/string appears
+
+    return 0;
+}
