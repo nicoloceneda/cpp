e361237016412e82f3827a9c8de9186b784adeda
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-23 10:32:47 +0100

    numbers

---
+++ 5-numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << 7+5 << endl;                                                                            // Addition
+    cout << 7-5 << endl;                                                                            // Subtraction
+    cout << 7*5 << endl;                                                                            // Multiplication
+    cout << 10/5 << endl;                                                                           // Division
+    cout << 10%3 << endl;                                                                           // Remainder
+
+    int nump = 5;                                                                                   // Add (subtract, multiply, divide) 1 to a number
+    nump++;
+    cout << nump << endl;         
+
+    int numpv = 5;                                                                                  // Add (subtract, multiply, divide) number to number
+    numpv += 2;
+    cout << numpv << endl;    
+
+    cout << 10/3 << endl;                                                                           // int op int returns int
+    cout << 10.0/3.0 << endl;                                                                       // double op double returns double
+    cout << 10.0/3 << endl;                                                                         // double op int returns double
+
+    return 0;
+}
---
+++ 6-operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()                                                                                          
+{
+    cout << pow(2,3) << endl;                                                                       // Power        
+    cout << sqrt(25) << endl;                                                                       // Square root     
+    cout << round(2.1) << endl;                                                                     // Rounding  
+    cout << ceil(2.1) << endl;                                                                      // Ceil
+    cout << floor(2.9) << endl;                                                                     // Floor 
+    cout << fmax(3,5) << endl;                                                                      // Maximum    
+    cout << fmin(3,5) << endl;                                                                      // Minimum                                                               
+
+    return 0;
+}
