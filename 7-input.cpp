7a10beda7ee1b282cf9fe685d7f1ca60ffb69247
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-23 21:55:17 +0100

    Inputs (incomplete)

---
+++ 7-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()                                                                                          
+{
+    int age;
+    cout << "Enter your age: " << endl;
+    cin >> age;                                                                                     // Oppositve of cout                                                                
+    cout << "You are " << age << " years old" << endl;
+
+    return 0;
+}
