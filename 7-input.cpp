0ef6619fdf32f8c598b799d6b57c39c2e507bf87
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-24 23:03:28 +0100

    madlib_game

--- 7-input.cpp
+++ 7-input.cpp
@@ -4,10 +4,31 @@ using namespace std;
 
 int main()                                                                                          
 {
+
     int age;
     cout << "Enter your age: " << endl;
-    cin >> age;                                                                                     // Oppositve of cout                                                                
+    cin >> age;                                                                                     // cin (oppositve of cout)                                                                                
     cout << "You are " << age << " years old" << endl;
+
+    cin.ignore();                                                                                  
+
+    char letter;
+    cout << "Enter your favorite letter: " << endl;
+    cin >> letter;                                                                                                                                                 
+    cout << "Your favorite letter is " << letter << endl;
+
+    cin.ignore(); 
+
+    string name;
+    cout << "Enter your full name: " << endl;
+    getline(cin, name);                                                                             // getline for strings                                                                                                        
+    cout << "Your full name is " << name << endl;
 
     return 0;
 }
+
+// When you input a character or integer with cin >>, it reads the input until it hits a whitespace 
+// or newline ('\n') character, but it doesn't consume that character. So, the newline character is 
+// still in the buffer. When getline(cin, name) executes, it reads until it hits a newline character, 
+// which is the first thing it encounters in the buffer, so it reads in an empty string. To fix this, 
+// you can use cin.ignore(), which will ignore or discard characters in the input buffer. 
---
+++ 8-calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double num1, num2;
+    cout << "Enter the first number" << endl;
+    cin >> num1;
+    cout << "Enter the second number" << endl;
+    cin >> num2;
+    cout << "The sum is " << num1 + num2 << endl;
+
+    return 0;
+}
