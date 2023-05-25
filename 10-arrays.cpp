0286f96d3d56e0594d44d1bcf31d3450b1189750
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-05-25 23:52:55 +0100

    arrays

--- 10-arrays.cpp
+++ 10-arrays.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
 
-    int numbers1[] = {1, 2, 3, 4, 5};                                                               // Container to store multiple elements
+    int numbers1[] = {1, 2, 3, 4, 5};                                                               // Elements provided directly (cannot add more elements)
 
     cout << numbers1[0] << endl;
 
-    int numbers2[6] = {1, 2, 3, 4, 5};                                                              // Specify the number of elements
+    int numbers2[6] = {1, 2, 3, 4, 5};                                                              // Specify the number of elements (can add more elements)
     numbers2[5] = 6;
     cout << numbers2[5] << endl;
 
