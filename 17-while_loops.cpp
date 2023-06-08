9da699c76b9a7424715a8532253b9d3c7bb6a4d4
Nicolo Ceneda <nicoloceneda@gmail.com>
2023-06-08 01:14:07 +0100

    while_loops

---
+++ 17-while_loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+string getDayOfWeek(int dayNum);
+
+int main()
+{   
+    int index = 1;
+
+    while (index <= 5) {                                                                            // Check condition and then execute if true
+
+        cout << index <<endl;
+        index++;
+    }
+
+    return 0;
+}
---
+++ 18-do_while_loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+string getDayOfWeek(int dayNum);
+
+int main()
+{   
+    int index = 1;
+
+    do {                                                                            // Execute if true and then check condition
+
+        cout << index <<endl;
+        index++;
+
+    } while (index <= 5);
+
+    return 0;
+}
